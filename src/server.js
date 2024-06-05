c07fec6b196023101e600e4f8cb7c54992398ed5
Julian Serrano <julius27egd@gmail.com>
2024-06-05 16:23:28 -0700

    final

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Ruta para servir el archivo HTML principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
